2a1edd65cf30cf885d53de42cbc884811df7d9b9
Dayson Pais <dayson@epicwhale.org>
2012-03-06 13:56:09 +0530

    Implemented findOne, findBy and findByTitle which return full nodes

--- lib/DrupalConnect/Repository/Node.php
+++ lib/DrupalConnect/Repository/Node.php
@@ -2,7 +2,11 @@
 namespace DrupalConnect\Repository;
 
 /**
- * Repository for Node Documents
+ * Repository for Node Documents.
+ *
+ * Fetching documents using the repository will always fetch all associated content with a node.
+ * This includes custom fields and fields like body.
+ *
  */
 class Node extends AbstractRepository
 {
@@ -12,16 +16,107 @@ class Node extends AbstractRepository
      */
     public function find($nid)
     {
-        $qb = $this->_dm->createQueryBuilder($this->_documentName);
+        $result = $this->createQueryBuilder()
+                       ->find()
+                       ->field('nid')->equals($nid)
+                       ->limit(1)
+                       ->getQuery()
+                       ->getSingleResult();
 
-        $result = $qb->find()
-                     ->field('nid')->equals($nid)
-                     ->getQuery()
-                     ->execute();
+        return $result;
+    }
 
-        if (!$result)
+    /**
+     * Query for one or more nodes based on several conditions that form a logical conjunction
+     *
+     * @param array $criteria
+     * @return array|null
+     */
+    public function findOneBy(array $criteria)
+    {
+        $qb = $this->createQueryBuilder()
+                   ->find()
+                   ->hydrate(false)
+                   ->select('nid')
+                   ->limit(1);
+
+        if ($criteria)
+        {
+            foreach ($criteria as $field => $val)
+            {
+                $qb->field($field)->equals($val);
+            }
+        }
+
+        $partialNode = $qb->getQuery()->getSingleResult();
+
+        if (!$partialNode)
             return null;
 
-        return $result->getSingleResult();
+        return $this->find($partialNode['nid']);
+    }
+
+    public function findBy(array $criteria)
+    {
+        // if only the nid criteria is set, then use the ->find($nid) for faster results (since only 1 request needed and 1 result expected)
+        if ($criteria && count($criteria) === 1 && isset($criteria['nid']))
+        {
+            $result = $this->find($criteria['nid']);
+            return ($result) ? array($result) : null;
+        }
+
+        $qb = $this->createQueryBuilder()
+                   ->find()
+                   ->hydrate(false)
+                   ->select('nid');
+
+        if ($criteria)
+        {
+            foreach ($criteria as $field => $val)
+            {
+                $qb->field($field)->equals($val);
+            }
+        }
+
+        $partialNodes = $qb->getQuery()->execute();
+
+        if (!$partialNodes)
+            return null;
+
+        $fullNodes = array();
+
+        foreach ($partialNodes as $pNode)
+        {
+            $fullNodes[] = $this->find($pNode['nid']);
+        }
+
+        return $fullNodes;
+
+    }
+
+    /**
+     * Find a Node by its title
+     *
+     * @param string $title
+     * @return null|\DrupalConnect\Document\Node
+     */
+    public function findOneByTitle($title)
+    {
+        return $this->findOneBy(array(
+            'title' => $title
+        ));
+    }
+
+    /**
+     * Find nodes by its title
+     *
+     * @param string $title
+     * @return null|\DrupalConnect\Document\Node[]
+     */
+    public function findByTitle($title)
+    {
+        return $this->findBy(array(
+            'title' => $title
+        ));
     }
 }
